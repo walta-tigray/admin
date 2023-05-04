d3e4d26355e0d35fe5c5075bf7b2565043610a05
RobelRH <robel.redda28@gmail.com>
2023-05-04 13:58:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import '@tremor/react/dist/esm/tremor.css';
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+        admin - portal
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/api_requests/url.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const baseURL = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_URL
+})
+
+baseURL.interceptors.request.use((req) => {
+  if (localStorage.getItem('auth-token')) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem('auth-token')}`;
+  }
+  return req;
+});
+
+export default baseURL
